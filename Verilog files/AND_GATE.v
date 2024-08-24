35731adcc17f3e1c3b9f9c056591f61edc036f50
manavshah-28 <manavbshah28@gmail.com>
2024-08-24 15:03:37 -0400

    bot pictures, fsm diagrams

--- Verilog files/AND_GATE.v
+++
@@ -1,8 +0,0 @@
-module AND_GATE
-(
-	input A,B,        // defining inputs A and B of AND gate 
-	output C          // defining output of AND gate
-);
-
-assign C = A & B;   // Logic implementation
-endmodule
